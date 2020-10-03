8b9c4c5f7f7de770185f74913736b4f408d95ade
Anibal Ventura <anibalventura20@gmail.com>
2020-10-03 19:09:34 -0400

    - Fix meta content. - Fix GitHub section. - Finish sections. - Change badges for new project.. - Add buttom to download resume. - Add resume file.

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,10 +63,8 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    // new GitHubCalendar("#github-graph", "anibalventura");
+    new GitHubCalendar("#github-graph", "anibalventura");
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "anibalventura", selector: "#ghfeed" });
-
-
 });
