f72284345732b18680716e7bc926b8e20366347a
Anibal Ventura <48188840+anibalventura@users.noreply.github.com>
2025-08-15 13:46:23 -0400

    fix: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  output: 'export',
-  trailingSlash: true,
-  basePath: '',
-  assetPrefix: '',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    output: 'export',
+    trailingSlash: true,
+    basePath: '',
+    assetPrefix: '',
+    distDir: 'out',
 }
 
 export default nextConfig
